fdde0d4cca106a9d14a6d3729f5e7d5880421ff7
Shing Lyu <shing.lyu@gmail.com>
2017-12-05 21:29:06 +0100

    Partially refactored extract_features.py

--- feature_extractor/extract_features.py
+++ feature_extractor/extract_features.py
@@ -2,25 +2,27 @@
 import config
 import feature_manager
 import sample_loader
+import json
 
 def main():
-   # FIXME: hardcoded input path
-   inputlist = "../data/corpus/split/test.list"
-   # We should probably extract all features, and let training script split it
+    # FIXME: hardcoded input/output path
+    inputlist = "../data/corpus/split/test.list"
+    training_features_filename = "./test_features.json"
+    # We should probably extract all features, and let training script split it
 
-   # TODO: maybe auto-search the folder?
-   training_samples = sample_loader.load_file_list(inputlist)
+    # TODO: maybe auto-search the folder?
+    with open(inputlist, 'r') as f:
+        sample_pathes = map(lambda x: x.strip(), f.readlines())
 
-   trainFeatsList = []
-   for trainSampFilename in trainSampList:
-      trainSamp = sampleLoader.loadTrainSample(trainSampFilename)
-      trainFeat = featureManager.extractTrainFeat(trainSamp)
-      trainFeatsList.append(trainFeat)
+    sample_loaders = [sample_loader.SampleLoader(path) for path in sample_pathes]
 
-   # trainFeatFilename = config.getTrainInFeatFilename(args)
-   trainFeatFilename = "./test_features.json"
-   # TODO: just use json here
-   featureManager.saveJson(trainFeatsList, trainFeatFilename);
+    samples = [feature_manager.loader.load_training_sample() for loader in sample_loaders]
+    training_features = [?]
+
+    # TODO: extract features
+
+    with open(training_features_filename, 'w') as f:
+        json.dump(training_features, f, indent=3)
 
 if __name__ == "__main__":
     main()
